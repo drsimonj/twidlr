4e5fec6b96527e594f54593e1734fc1f089fea2c
drsimonj <drsimonjackson@gmail.com>
2017-04-23 08:58:09 +1000

    Fix: unpacking data from formula
    Function for unpacking data from formula made more robust

--- R/utilities.R
+++ R/utilities.R
@@ -18,20 +18,28 @@
 #' unpack_formula(y ~ .)
 #' unpack_formula(~ "x")
 unpack_formula <- function(f) {
-  lhs <- as.character(lazyeval::f_lhs(f))
-  rhs <- as.character(lazyeval::f_rhs(f))
+  if (!is(f, "formula"))
+    stop("Please provide a formula object such as `y ~ x`")
+
+  lhs <- c()
+  rhs <- all.vars(f[[2]])
+  if(length(f) == 3) {
+    lhs <- rhs
+    rhs <- all.vars(f[[3]])
+  }
+
   list(lhs = lhs, rhs = rhs)
 }
 
 #' Convert a data frame and formula to input matrix and output vector
 #'
-#' Convert a data frame and formula of the form "\code{output ~ feature_1 +
-#' feature_2 + ... + feature_i}" to a list containing all input variables as a
-#' matrix and the output variable as a vector. These structures are commonly
-#' used by various modelling functions such as \code{\link[glmnet]{glmnet}}
+#' Convert a data frame and formula of the form "\code{y ~ x1 + x2 + ... + xi}"
+#' to a list containing all input variables as a matrix and the output variable
+#' as a vector. These structures are commonly used by various modelling
+#' functions such as \code{\link[glmnet]{glmnet}}
 #'
 #' @param data A data frame
-#' @param f A formula of the form \code{output ~ feature_1 + feature_2 + ... + feature_i}
+#' @param formula A formula of the form \code{y ~ x1 + x2 + ... + xi}
 #'
 #' @return Two-element list containing output values as a vector named \code{y}
 #'   and an input matrix called \code{X}
@@ -39,17 +47,23 @@ unpack_formula <- function(f) {
 #' @export
 #'
 #' @examples
+#' unpack_data(mtcars, hp ~ mpg + wt + disp)
 #' unpack_data(mtcars, hp ~ .)
+#' unpack_data(mtcars, ~ .)
 unpack_data <- function(data, f) {
   # Extract variables
-  vars <- unpack_formula(f)
-  if (identical(vars$rhs, '.'))
-    vars$rhs <- names(data)[names(data) != vars$lhs]
-  vars$rhs <- vars$rhs[vars$rhs != "+"]
+  v <- unpack_formula(f)
+  if (identical(v$rhs, '.')) {
+    v$rhs <- names(data)
+    if (!is.null(v$lhs))
+      v$rhs <- v$rhs[v$rhs != v$lhs]
+  }
 
   # Convert data frame to input matrix and label vector
-  X <- data.matrix(data[, vars$rhs])
-  y <- data[[vars$lhs]]
+  y <- NULL
+  if (!is.null(v$lhs))
+    y <- data[[v$lhs]]
+  X <- data.matrix(data[v$rhs])
 
   list(y = y, X = X)
 }
