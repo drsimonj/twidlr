5e2e64ccdda84692b18754433b081e9f0e142976
drsimonj <drsimonjackson@gmail.com>
2017-04-23 08:08:31 +1000

    Fix: remove model saving from xgboost test
    The xgboost test was saving a model each time. This commit added the appropriate argument to stop this from happening.

--- tests/testthat/test-xgboost.R
+++ tests/testthat/test-xgboost.R
@@ -1,11 +1,11 @@
 context("xgboost")
 
 test_that("== xgboost output", {
-  twidlr_fit <- twidlr::xgboost(mtcars, mpg ~ ., nrounds = 5, verbose = 0)
+  twidlr_fit <- twidlr::xgboost(mtcars, mpg ~ ., nrounds = 5, verbose = 0, save_period = NULL)
 
   data = data.matrix(mtcars[-1])
   label = mtcars[[1]]
-  xgboost_fit <- xgboost::xgboost(data = data, label = label, nrounds = 5, verbose = 0)
+  xgboost_fit <- xgboost::xgboost(data = data, label = label, nrounds = 5, verbose = 0, save_period = NULL)
 
   expect_equal(
     predict(twidlr_fit,  data),
