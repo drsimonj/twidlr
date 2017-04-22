a6b5376e2f76dceb95d61059f8f518fde8105545
drsimonj <drsimonjackson@gmail.com>
2017-04-22 17:44:56 +1000

    Add: method for xgboost::xgboost

--- R/lm.R
+++ R/lm.R
@@ -1,4 +1,4 @@
-#' data.frame-first formula-second method of stats::lm
+#' data.frame-first formula-second method for stats::lm
 #'
 #' This function passes a data.frame, formula, and additional arguments to
 #' \code{\link[stats]{lm}}().
@@ -20,8 +20,9 @@ lm <- function(data, formula, ...) {
 
 #' @export
 lm.default <- function(data, formula, ...) {
-  stop("twidlr::lm() requires the first argument `data` to be a data frame. ",
-       "If you are attempting to use lm() normally, use `stats::lm()`")
+  #stop("twidlr::lm() requires the first argument `data` to be a data frame. ",
+  #     "If you are attempting to use lm() normally, use `stats::lm()`")
+  lm.data.frame(as.data.frame(data), formula, ...)
 }
 
 #' @export
---
+++ R/xgboost.R
@@ -0,0 +1,39 @@
+#' data.frame-first formula-second method for xgboost::xgboost
+#'
+#' This function passes a data.frame, formula, and additional arguments to
+#' \code{\link[xgboost]{xgboost}}().
+#'
+#' @seealso \code{\link[xgboost]{xgboost}}
+#'
+#' @param data Data frame containing the variables in the model.
+#' @param formula A formula of the form \code{label ~ feature_1 + feature_2 +
+#'   ... + feature_i}
+#' @param ... additional arguments to pass to \code{\link[xgboost]{xgboost}}()
+#'
+#' @export
+#'
+#' @examples
+#' xgboost(mtcars, hp ~ ., nrounds = 10)
+xgboost <- function(data, formula, ...) {
+  UseMethod("xgboost")
+}
+
+#' @export
+xgboost.default <- function(data, formula, ...) {
+  xgboost.data.frame(as.data.frame(data), formula, ...)
+}
+
+#' @export
+xgboost.data.frame <- function(data, formula, ...) {
+  # Extract variables
+  vars <- unpack_formula(formula)
+  if (identical(vars$rhs, '.'))
+    vars$rhs <- names(data)[names(data) != vars$lhs]
+  vars$rhs <- vars$rhs[vars$rhs != "+"]
+
+  # Convert data frame to input matrix and label vector
+  X <- data.matrix(data[, vars$rhs])
+  y <- data[[vars$lhs]]
+
+  xgboost::xgboost(data = X, label = y, ...)
+}
