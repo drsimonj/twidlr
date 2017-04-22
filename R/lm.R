780104b55e6dda504dc0daf03e4c151c35b98ecb
drsimonj <drsimonjackson@gmail.com>
2017-04-22 16:46:32 +1000

    Update: docs for lm method

--- R/lm.R
+++ R/lm.R
@@ -3,7 +3,17 @@
 #' This function passes a data.frame, formula, and additional arguments to
 #' \code{\link[stats]{lm}}().
 #'
+#' @seealso \code{\link[stats]{lm}}
+#'
+#' @param data Data frame containing the variables in the model.
+#' @inheritParams stats::lm
+#' @param ... additional arguments to pass to \code{\link[stats]{lm}}()
+#'
 #' @export
+#'
+#' @examples
+#' fit <- lm(mtcars, hp ~ .)
+#' summary(fit)
 lm <- function(data, formula, ...) {
   UseMethod("lm")
 }
