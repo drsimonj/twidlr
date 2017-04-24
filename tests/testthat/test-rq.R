9f73cf066f04f51787ed8cf06185f58d57bd2404
joran <joran.elias@gmail.com>
2017-04-23 18:50:51 -0600

    tweak rq test so as not to use named args

--- tests/testthat/test-rq.R
+++ tests/testthat/test-rq.R
@@ -2,10 +2,10 @@ context("rq")
 
 test_that("== quantreg output", {
   expect_equal(coef(twidlr::rq(mtcars, hp ~ mpg + cyl)),
-               coef(quantreg::rq(hp ~ mpg + cyl, data = mtcars)))
+               coef(quantreg::rq(hp ~ mpg + cyl,tau = 0.5,mtcars)))
 })
 
 test_that("== quantreg output", {
   expect_equal(coef(twidlr::rq(mtcars, hp ~ mpg + cyl,tau = c(0.25,0.75))),
-               coef(quantreg::rq(hp ~ mpg + cyl,tau = c(0.25,0.75),data = mtcars)))
+               coef(quantreg::rq(hp ~ mpg + cyl,c(0.25,0.75),mtcars)))
 })
