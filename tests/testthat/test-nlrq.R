5d4222f6e73c36d4d265b90d98b65abc041ad87c
drsimonj <drsimonjackson@gmail.com>
2017-04-25 21:07:18 +1000

    Add: predict and test for nlrq

--- tests/testthat/test-nlrq.R
+++ tests/testthat/test-nlrq.R
@@ -5,7 +5,13 @@ Dat$x <- rep(1:25, 20)
 set.seed(1)
 Dat$y <- SSlogis(Dat$x, 10, 12, 2) * rnorm(500, 1, 0.1)
 
-test_that("== quantreg::nlrq output", {
-  expect_equal(coef(twidlr::nlrq(Dat,y ~ SSlogis(x, Asym, mid, scal))),
-               coef(quantreg::nlrq(y ~ SSlogis(x, Asym, mid, scal),Dat)))
+twidlr_fit <- twidlr::nlrq(Dat,y ~ SSlogis(x, Asym, mid, scal))
+origin_fit <- quantreg::nlrq(y ~ SSlogis(x, Asym, mid, scal),Dat)
+
+test_that("fit", {
+  expect_equal(coef(twidlr_fit), coef(origin_fit))
+})
+
+test_that("predict", {
+  expect_equal(predict(twidlr_fit), quantreg::predict.nlrq(origin_fit))
 })
