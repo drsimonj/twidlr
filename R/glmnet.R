596931862c2921285562d2978f2f26b887b1c262
drsimonj <drsimonjackson@gmail.com>
2017-04-25 13:58:51 +1000

    Add: predict method for glment

--- R/glmnet.R
+++ R/glmnet.R
@@ -26,5 +26,29 @@ glmnet.default <- function(data, formula, ...) {
 #' @export
 glmnet.data.frame <- function(data, formula, ...) {
   dat <- model_as_xy(data, formula)
-  glmnet::glmnet(x = dat$x, y = dat$y, ...)
+  object <- glmnet::glmnet(x = dat$x, y = dat$y, ...)
+
+  # Set up for custom predict function
+  class(object) <- c("twidlr_glmnet", class(fit))
+  attr(object, "formula") <- formula
+  fit
+}
+
+#' twildr compatible predict for \code{\link[glmnet]{glmnet}}
+#'
+#' @export
+predict.twidlr_glmnet <- function(object, data, ...) {
+  if (hasArg(newx)) {
+    stop("twidlr replaces 'newx' with 'data'. Please see examples in ?twidlr")
+  }
+
+  if (missing(data)) {
+    if (hasArg(type) && list(...)["type"] %in% c("coefficients", "nonzero")) {
+      return (NextMethod("predict", object, ...))
+    }
+    stop ("You need to supply a value for 'data'")
+  }
+
+  data <- model_as_xy(data, attr(object, "formula"))$x
+  NextMethod("predict", object, newx = data, ...)
 }
