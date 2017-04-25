b3d667dc70aba3a0dd1af80680bdc625ce3588a8
drsimonj <drsimonjackson@gmail.com>
2017-04-25 21:32:55 +1000

    Comment out rqss
    qss is not available without loading of quantreg, which causes conflicts (e.g, with predict). This must be dealt with in order to properly implement this function

--- R/rqss.R
+++ R/rqss.R
@@ -1,35 +1,43 @@
-#' data.frame-first formula-second method for \code{\link[quantreg]{rqss}}
+#' #' data.frame-first formula-second method for \code{\link[quantreg]{rqss}}
+#' #'
+#' #' This function passes a data.frame, formula, and additional arguments to
+#' #' \code{\link[quantreg]{rqss}}.
+#' #'
+#' #' @seealso \code{\link[quantreg]{rqss}}
+#' #'
+#' #' @inheritParams twidlr_defaults
+#' #' @export
+#' #'
+#' #' @examples
+#' #' n <- 200
+#' #' x <- sort(rchisq(n,4))
+#' #' z <- x + rnorm(n)
+#' #" y <- log(x)+ .1*(log(x))^2 + log(x)*rnorm(n)/4 + z
+#' #' dat <- data.frame(y = y,x = x,z = z)
+#' #' rm(x,y,z)
+#' #' fit  <- rqss(dat,y ~ qss(x, constraint= "N") + z,tau = 0.5)
+#' #'
+#' #' # Help page for function being twiddled
+#' #' ?quantreg::rqss
+#' rqss <- function(data, formula, ...) {
+#'   check_pkg("quantreg")
+#'   UseMethod("rqss")
+#' }
 #'
-#' This function passes a data.frame, formula, and additional arguments to
-#' \code{\link[quantreg]{rqss}}.
+#' #' @export
+#' rqss.default <- function(data, formula, ...) {
+#'   rqss.data.frame(as.data.frame(data), formula, ...)
+#' }
 #'
-#' @seealso \code{\link[quantreg]{rqss}}
+#' #' @export
+#' rqss.data.frame <- function(data, formula, ...) {
+#'   quantreg::rqss(formula = formula, data = data, ...)
+#' }
 #'
-#' @inheritParams twidlr_defaults
-#' @export
-#'
-#' @examples
-#' n <- 200
-#' x <- sort(rchisq(n,4))
-#' z <- x + rnorm(n)
-#" y <- log(x)+ .1*(log(x))^2 + log(x)*rnorm(n)/4 + z
-#' dat <- data.frame(y = y,x = x,z = z)
-#' rm(x,y,z)
-#' fit  <- rqss(dat,y ~ qss(x, constraint= "N") + z,tau = 0.5)
-#'
-#' # Help page for function being twiddled
-#' ?quantreg::rqss
-rqss <- function(data, formula, ...) {
-  check_pkg("quantreg")
-  UseMethod("rqss")
-}
-
-#' @export
-rqss.default <- function(data, formula, ...) {
-  rqss.data.frame(as.data.frame(data), formula, ...)
-}
-
-#' @export
-rqss.data.frame <- function(data, formula, ...) {
-  quantreg::rqss(formula = formula, data = data, ...)
-}
+#' #' @rdname rqss
+#' #' @export predict.rqss
+#' predict.rqss <- function(object, data, ...) {
+#'   if (hasArg(newdata)) data <- newdata
+#'   if (missing(data)) stop("argument 'data' is missing, with no default")
+#'   quantreg::predict.rqss(object, newdata = data, ...)
+#' }
