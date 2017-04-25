3b2d231388af674f8b2d386fc1b7331bd059723d
drsimonj <drsimonjackson@gmail.com>
2017-04-25 21:31:39 +1000

    Add: predict and test for rq

--- tests/testthat/test-rq.R
+++ tests/testthat/test-rq.R
@@ -1,11 +1,12 @@
 context("rq")
 
-test_that("== quantreg output", {
-  expect_equal(coef(twidlr::rq(mtcars, hp ~ mpg + cyl)),
-               coef(quantreg::rq(hp ~ mpg + cyl,tau = 0.5,mtcars)))
+twidlr_fit <- twidlr::rq(mtcars, hp ~ mpg + cyl)
+origin_fit <- quantreg::rq(hp ~ mpg + cyl,tau = 0.5,mtcars)
+
+test_that("fit", {
+  expect_equal(coef(twidlr_fit), coef(origin_fit))
 })
 
-test_that("== quantreg output", {
-  expect_equal(coef(twidlr::rq(mtcars, hp ~ mpg + cyl,tau = c(0.25,0.75))),
-               coef(quantreg::rq(hp ~ mpg + cyl,c(0.25,0.75),mtcars)))
+test_that("predict", {
+  expect_equal(predict(twidlr_fit), quantreg::predict.rq(origin_fit))
 })
