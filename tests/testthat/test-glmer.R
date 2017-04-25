f9d509a575cb62f354fab4f9ae06de29a6c84c4b
Lingtax <Lingtax@users.noreply.github.com>
2017-04-25 16:17:50 +1000

    Correct reference to cbpp data
    replace all 'cbpp' with 'lme4::cbpp'

--- tests/testthat/test-glmer.R
+++ tests/testthat/test-glmer.R
@@ -1,6 +1,6 @@
 context("glmer")
 
 test_that("== lme4 output", {
-  expect_equal(stats::coef(twidlr::glmer(cbpp, cbind(incidence, size - incidence) ~ period + (1 | herd), family = binomial)),
-               stats::coef(lme4::glmer(cbind(incidence, size - incidence) ~ period + (1 | herd),cbpp, family = binomial)))
+  expect_equal(stats::coef(twidlr::glmer(lme4::cbpp, cbind(incidence, size - incidence) ~ period + (1 | herd), family = binomial)),
+               stats::coef(lme4::glmer(cbind(incidence, size - incidence) ~ period + (1 | herd), lme4::cbpp, family = binomial)))
 })
