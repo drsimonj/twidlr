ae690b92b46d792aeb4bd0a52ba25f894230b698
drsimonj <drsimonjackson@gmail.com>
2017-04-25 17:41:40 +1000

    Fix: glm predict and test

--- tests/testthat/test-glm.R
+++ tests/testthat/test-glm.R
@@ -1,9 +1,13 @@
 context("glm")
 
-test_that("== stats output", {
-  expect_equal(stats::coef(twidlr::glm(mtcars, vs ~ hp + wt, family = binomial())),
-               stats::coef(stats::glm(vs ~ hp + wt, family = binomial(), mtcars)))
+twidlr_fit <- twidlr::glm(mtcars, vs ~ hp + wt, family = binomial())
+origin_fit <- stats::glm(vs ~ hp + wt, family = binomial(), mtcars)
+
+test_that("fit", {
+  expect_equal(coef(twidlr_fit), coef(origin_fit))
 })
 
-
+test_that("predict", {
+  expect_equal(predict(twidlr_fit), predict(origin_fit))
+})
 
