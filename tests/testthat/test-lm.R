c4c5002d5d5246cc58008def805748c013f5ad60
drsimonj <drsimonjackson@gmail.com>
2017-04-25 17:37:08 +1000

    Fix: test convention
    Rather than comparing twidlr_fit to <package>_fit, make the latter origin_fit to be same in all test files

--- tests/testthat/test-lm.R
+++ tests/testthat/test-lm.R
@@ -1,12 +1,12 @@
 context("lm")
 
 twidlr_fit <- twidlr::lm(mtcars, hp ~ .)
-stats_fit  <- stats::lm(hp ~ ., mtcars)
+origin_fit <- stats::lm(hp ~ ., mtcars)
 
 test_that("fit", {
-  expect_equal(coef(twidlr_fit), coef(stats_fit))
+  expect_equal(coef(twidlr_fit), coef(origin_fit))
 })
 
 test_that("predict", {
-  expect_equal(predict(twidlr_fit), predict(stats_fit))
+  expect_equal(predict(twidlr_fit), predict(origin_fit))
 })
