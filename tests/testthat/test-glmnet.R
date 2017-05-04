50f1ce05f0ecfb8517513b837b6b0d869f421ccb
drsimonj <drsimonjackson@gmail.com>
2017-05-04 16:50:40 +1000

    Add: glmnet::cv.glmnet twidlr

--- tests/testthat/test-glmnet.R
+++ tests/testthat/test-glmnet.R
@@ -14,3 +14,19 @@ test_that("glmnet", {
   expect_equal(predict(twidlr_fit, data = d),
                glmnet::predict.glmnet(origin_fit, newx = x))
 })
+
+test_that("cv.glmnet", {
+  d <- datasets::mtcars
+  x <- model.matrix(hp ~ ., mtcars)[,-1]
+  y <- mtcars$hp
+
+  set.seed(170504)
+  twidlr_fit <- twidlr::cv.glmnet(mtcars, hp ~ .)
+  set.seed(170504)
+  origin_fit <- glmnet::cv.glmnet(x, y)
+
+  expect_equal(coef(twidlr_fit, s = "lambda.min"), coef(origin_fit, s = "lambda.min"))
+  expect_error(predict(twidlr_fit))
+  expect_equal(predict(twidlr_fit, data = d, s = "lambda.min"),
+               glmnet::predict.cv.glmnet(origin_fit, newx = x, s = "lambda.min"))
+})
