aec37ab6bc13db9d9e025be314c8f17163e64fc8
drsimonj <drsimonjackson@gmail.com>
2017-05-12 15:50:38 +1000

    Add: e1071::svm twidlr

---
+++ R/twidlr-e1071.R
@@ -0,0 +1,29 @@
+#' data.frame-first formula-second method for \code{\link[e1071]{svm}}
+#'
+#' This function passes a data.frame, formula, and additional arguments to
+#' \code{\link[e1071]{svm}}.
+#'
+#' @seealso \code{\link[e1071]{svm}}
+#'
+#' @inheritParams twidlr_defaults
+#' @formula \code{\link[stats]{formula}} (or object that can be coerced to one)
+#'   describing the model to be fitted. If one-sided, density-estimation is
+#'   carried out. If omitted, density-estimation is done on all variables in
+#'   `data`.
+#' @export
+#'
+svm <- function(data, formula = ~., ...) {
+  check_pkg("e1071")
+  UseMethod("svm")
+}
+
+#' @export
+svm.default <- function(data, formula = ~., ...) {
+  key_args <- coerce_args(data, formula)
+  data     <- key_args$data
+  formula  <- key_args$formula
+
+  e1071:::svm.formula(formula = formula, data = data, ...)
+}
+
+
