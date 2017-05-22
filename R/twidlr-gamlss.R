3fc3d56d4a2b173c4a57008d51a02c357e09fce8
drsimonj <drsimonjackson@gmail.com>
2017-05-22 17:11:40 +1000

    Clean: gamlss code

--- R/twidlr-gamlss.R
+++ R/twidlr-gamlss.R
@@ -1,16 +1,12 @@
-
-
-
 #' data.frame-first formula-second method for \code{\link[gamlss]{gamlss}}
 #'
 #' This function passes a data.frame, formula, and additional arguments to
 #' \code{\link[gamlss]{gamlss}}.
 #'
-#'This function attaches the training data to the generated model,
-#'because the default gamlss.predict() function requires access to
-#'the original data in order to make predictions. Please be aware
-#'that memory issues can arise when working with large data sets and
-#'or when generating a large number of models.
+#' This function attaches the training data to the generated model, because the
+#' default gamlss.predict() function requires access to the original data in
+#' order to make predictions. Please be aware that memory issues can arise when
+#' working with large data sets and or when generating a large number of models.
 #'
 #' @seealso \code{\link[gamlss]{gamlss}}
 #'
@@ -21,8 +17,6 @@
 #' fit <- gamlss(mtcars, vs ~ hp + wt, family = "BI")
 #' summary(fit)
 #' predict(fit, mtcars)
-#'
-
 gamlss <- function(data, formula, ...) {
   check_pkg("gamlss")
   UseMethod("gamlss")
@@ -30,31 +24,23 @@ gamlss <- function(data, formula, ...) {
 
 #' @export
 gamlss.default <- function(data, formula, ...) {
-
   key_args <- coerce_args(data, formula)
   data     <- key_args$data
   formula  <- key_args$formula
 
   original_data <- data[, all.vars(formula)]
-
-  fit = gamlss::gamlss(formula = formula, data = original_data, ...)
-
-  attr(fit, "original_data") <- original_data
-
-  return(fit)
+  object <- gamlss::gamlss(formula = formula, data = original_data, ...)
+  attr(object, "original_data") <- original_data
+  object
 }
 
 #' @rdname gamlss
 #' @export
 #' @export predict.gamlss
 predict.gamlss <- function(object, data, ...) {
-
   data <- predict_checks(data = data, ...)
-
-  original_data = attr(object, "original_data")
-
-  adapted_data = data[, names(data) %in% names(original_data) ]
-
+  original_data <- attr(object, "original_data")
+  adapted_data  <- data[names(original_data)]
   gamlss:::predict.gamlss(object, newdata = adapted_data, data = original_data, ...)
 }
 
--- tests/testthat/test-gamlss.R
+++ tests/testthat/test-gamlss.R
@@ -1,12 +1,11 @@
-
 context("gamlss")
 
 
 test_that("gamlss", {
   d <- datasets::mtcars
 
-  twidlr_fit <- twidlr::gamlss(d, vs ~ hp + wt, family = "BI" )
-  origin_fit <- gamlss::gamlss(vs ~ hp + wt, data = d, family = "BI" )
+  twidlr_fit <- twidlr::gamlss(d, vs ~ hp + wt, family = "BI", trace = FALSE)
+  origin_fit <- gamlss::gamlss(vs ~ hp + wt, data = d, family = "BI", trace = FALSE)
 
   expect_equal(coef(twidlr_fit), coef(origin_fit))
   expect_error(predict(twidlr_fit))
